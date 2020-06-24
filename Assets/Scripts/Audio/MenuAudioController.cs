7753fd0c5c5c39ac8d2cb98d42f2b6916009fe97
zacharysarette <zacharywsarette@gmail.com>
2020-06-25 00:42:09 +0900

    Adds assets and sets up main game music.

--- Assets/Scripts/Audio/MenuAudioController.cs
+++ Assets/Scripts/Audio/MenuAudioController.cs
@@ -5,8 +5,15 @@ using UnityEngine;
 public class MenuAudioController : MonoBehaviour
 {
     public AudioClip music;
+
+    public AudioClip gameMusic;
     void Start()
     {
         AudioManager.Instance.PlayMusicWithFade(music);
+    }
+
+    public void playGameMusic()
+    {
+        AudioManager.Instance.PlayMusicWithCrossFade(gameMusic);
     }
 }
