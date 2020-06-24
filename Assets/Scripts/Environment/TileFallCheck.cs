6a81369ca82f9facafbd3ab3fad97ed534d7abe7
Lewis Whiteman <lewiswhiteman@gmail.com>
2020-06-24 14:53:32 +0100

    Falling gravel now kills enemies

--- Assets/Scripts/Environment/TileFallCheck.cs
+++ Assets/Scripts/Environment/TileFallCheck.cs
@@ -5,35 +5,41 @@ using UnityEngine;
 public class TileFallCheck : MonoBehaviour
 {
     public bool isFalling;
-    // Start is called before the first frame update
     void Start() => StartCoroutine(DoFallCheck());
     private IEnumerator DoFallCheck()
     {
         while (true)
         {
-            if (GenerateEnvironment.instance != null && transform.position.y < GenerateEnvironment.minBound)
-            {
-                Destroy(gameObject);
-                yield break;
-            }
-
             var topHits = Physics2D.RaycastAll(transform.position, Vector2.up, 1.0f);
-            var bottomHits = Physics2D.RaycastAll(transform.position, -Vector2.up,1.0f);
+            var bottomHits = Physics2D.RaycastAll(transform.position, -Vector2.up, 1.0f);
 
+            // Nothing above and below
             if (topHits.Length == 0 && bottomHits.Length == 0)
             {
                 transform.position -= Vector3.up;
-                GameAudioController.playDroppingCrates();
                 isFalling = true;
+                GameAudioController.playDroppingCrates();
             }
+            // Gravel Tile above, nothing below
             else if (topHits.Length == 1 && topHits[0].collider.gameObject.TryGetComponent<TileFallCheck>(out var _) && bottomHits.Length == 0)
             {
                 transform.position -= Vector3.up;
-                GameAudioController.playDroppingCrates();
-                isFalling = true;
-            }
-            else
                 isFalling = false;
+            }
+            // Nothing above, Enemy below
+            else if (topHits.Length == 0 && bottomHits.Length == 1 && bottomHits[0].collider.CompareTag("Enemy"))
+            {
+                transform.position -= Vector3.up;
+                isFalling = true;
+                GameAudioController.playDroppingCrates();
+            }
+            // Enemy above, nothing below
+            else if (topHits.Length == 1 && bottomHits.Length == 0 && topHits[0].collider.CompareTag("Enemy"))
+            {
+                transform.position -= Vector3.up;
+                isFalling = true;
+                GameAudioController.playDroppingCrates();
+            }
 
             yield return new WaitForSeconds(0.25f);
         }
